1f3f0eb54448ba6cd976871b5e3d014acd1523b1
Oana <oan.max@gmail.com>
2020-05-21 14:45:22 +0300

    add Movie model 1.0

---
+++ MoviesApp/Models/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApp.Models
+{
+    public enum MovieGenre
+    {
+        action = 0, 
+        comedy = 1,
+        horror = 2,
+        thriller = 3
+    }
+}
---
+++ MoviesApp/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApp.Models
+{
+    public class Movie
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public MovieGenre Genre { get; set; }      
+        public string Duration { get; set; }    // durata -> metoda de masurare?
+        public string YearOfRelease { get; set; }   // time format for year
+        public string Director { get; set; }
+        public DateTimeOffset DateAdded { get; set; }
+        public int Rating { get; set; }
+        public bool Watched { get; set; }       //yes,no
+    }
+}
